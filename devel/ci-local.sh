58a8fa8bddfab0fd492da177bc1f9c0eaafc2918
Philipp Schmitt <philipp@schmitt.co>
2020-12-01 20:16:38 +0100

    Update ci-local.sh
    - Add usage() [--help]
    - Add formatting shorthand for executing black + isort
    - Fix shebang

--- devel/ci-local.sh
+++ devel/ci-local.sh
@@ -1,27 +1,54 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+usage() {
+  echo "Usage: $(basename "$0") [CHECK]"
+  echo
+  echo "Valid CHECK values:"
+  echo "  - all [default]"
+  echo "  - black"
+  echo "  - isort"
+  echo "  - formatting (black + isort)"
+  echo "  - flakehell"
+  echo "  - mypy"
+}
+
+# shellcheck disable=2120
 check_black() {
-  poetry run black .
+  poetry run black "$@" .
 }
 
+# shellcheck disable=2120
 check_isort() {
-  poetry run isort .
+  poetry run isort "$@" .
 }
 
+# shellcheck disable=2120
 check_flakehell() {
-  poetry run flakehell lint --format=stat --count --max-complexity=10 --max-line-length=80
+  poetry run flakehell lint \
+    --format=stat \
+    --count \
+    --max-complexity=10 \
+    --max-line-length=80 \
+    "$@"
 }
 
+# shellcheck disable=2120
 check_mypy() {
-  poetry run python -m mypy .
+  poetry run python -m mypy "$@" .
 }
 
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
+  # Go to project root
   cd "$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)/.." || exit 9
 
-  set -ex
+  set -e
+
+  if [[ -n "$DEBUG" ]]
+  then
+    set -x
+  fi
 
   check_name="all"
 
@@ -30,9 +57,25 @@ then
       check_name="flakehell"
       shift
       ;;
-    black|isort|mypy)
+    mypy|types)
+      check_name="mypy"
+      shift
+      ;;
+    black|isort)
       check_name="$1"
       shift
+      ;;
+    format|formatting)
+      check_name="formatting"
+      shift
+      ;;
+    help|--help|h|-h)
+      usage
+      exit 0
+      ;;
+    *)
+      usage >&2
+      exit 2
       ;;
   esac
 
@@ -43,6 +86,11 @@ then
     check_isort
     check_flakehell
     check_mypy
+  elif [[ "$check_name" == "formatting" ]]
+  then
+    echo "Running formatting checks" >&2
+    check_black
+    check_isort
   else
     "check_${check_name}" "$@"
   fi
